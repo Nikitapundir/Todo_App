c208b2de251c680b6d32b9c47501a7f96c1ec698
NikitaPundir <nikitapundir95@gmail.com>
2017-07-21 12:08:25 +0530

    Todo's list is being displayed

--- main.js
+++ main.js
@@ -2,4 +2,27 @@ var ngtodo = angular.module('ngtodo' ,[]);
 // head of department
 ngtodo.controller('mainController' , function($scope){
 	console.log($scope)
+	$scope.ListofTodos=[
+	{
+		title: "Do my homework",
+		details:"I must do my homework"
+	},
+	{
+		title: "Do my homework",
+		details:"I must do my homework"
+	},
+	{
+        title: "Do my homework",
+		details:"I must do my homework"
+
+	},
+	{
+		title: "Do my homework",
+		details:"I must do my homework"
+	},
+	{
+		title: "Do my homework",
+		details:"I must do my homework"
+	}
+	];
 });
